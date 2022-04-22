9c84f4d80bea18abf1481089175fea1700d0000a
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-04-21 20:16:26 -0500

    add: Express routes

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.get('/', (req,res) => {
     res.send('Hello World')
 })
 
+// Respondiendo texto
+// localhost:3000/launchx
+app.get('/launchx', (req, res) => {
+    res.send('Bienvenidos a launchx')
+})
+
+// Regresando un objeto
+// localhost:3000/explorersInNode
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {name: "Explorer", msg: "Hello"}
+    res.send(explorer)
+})
+
 //Con esto inicializamos esta app
 app.listen(port,() => {
     console.log(`Example app listening on port ${port}`)
